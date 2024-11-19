759dc229260c613966d56d16af0ef06d3b286c11
ebby-jane <ebere2156@gmail.com>
2024-11-19 15:14:23 +0100

    complete assets work.html about.html index.html profile.html blog.html education.html contact.html

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     $("h1").css("color", "brown")
+//     $("a").attr("href", "https://www.ebere2156@gmail.com")
+// })
+
+$(".header-text").on("click", function () {
+    $("h1").slideUp().slideDown()
+})
+
